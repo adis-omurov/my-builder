588e8a00db738b655dc9ddfa092a7af5c49aa33c
Your Name <you@example.com>
2021-05-31 14:09:38 +0600

    nav moving

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import classes from './CheckoutForm.module.css';
+import Button from "../../UI/Button/Button"
+import classes from "./CheckoutForm.module.css"
 
-function CheckoutForm(props) {
+const CheckoutForm = ({ cancelCallback, submitCallback }) => {
   return (
-    <div className={classes.CheckoutForm}>
-      <h3>Checkout</h3>
-
-      <label>Name: <input
-        type="text"
-        placeholder="e.g. Bakyt"
-        onChange={event => props.changeHandler('name', event.target.value)} /></label>
-      <label>Phone: <input
-        type="text"
-        placeholder="e.g. 0 555 123 456"
-        onChange={event => props.changeHandler('phone', event.target.value)} /></label>
-      <label>Address: <input
-        type="text"
-        placeholder="e.g. 150 Karasaeva str."
-        onChange={event => props.changeHandler('address', event.target.value)} /></label>
-      
-      <button onClick={props.submitHandler}>Submit</button>
-      <button onClick={props.cancelHandler}>Cancel</button>
-    </div>
+    <form className={classes.CheckoutForm} onSubmit={submitCallback}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+      <div>
+        <label htmlFor="address">Address</label>
+        <input type="text" name="address" id="address" required />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone</label>
+        <input type="text" name="phone" id="phone" required pattern="0[0-9]{9}" />
+      </div>
+      <Button>Checkout</Button>
+      <Button onClick={cancelCallback}>Cancel</Button>
+    </form>
   );
 }
-
+ 
 export default CheckoutForm;
