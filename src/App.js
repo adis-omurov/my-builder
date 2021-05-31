ac563ddc61fa244f636d9a6aebfbc17cd6d07bcd
Your Name <you@example.com>
2021-05-31 13:43:09 +0600

    App js crs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import Layout from "./components/Layout/Layout";
+import PizzaBuilder from "./components/PizzaBuilder/PizzaBuilder";
 
-import Layout from './containers/Layout/Layout';
-import PizzaBuilder from './containers/PizzaBuilder/PizzaBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
+import "./App.css";
+import { Redirect, Route, Switch } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { restore } from "./store/actions/auth";
+import { useEffect } from "react";
+import Logout from "./components/Logout/Logout";
+import Auth from "./components/Auth/Auth";
+import Orders from "./components/Orders/Orders";
+import Checkout from "./components/Checkout/Checkout";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Layout>
-            <Route path="/" component={PizzaBuilder} exact />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-          </Layout>
-        </BrowserRouter>
-      </div>
-    );
+const App = () => {
+  const disptach = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  useEffect(() => {
+    disptach(restore());
+  }, [disptach]);
+
+  let output = (
+    <Switch>
+      <Route path="/" component={PizzaBuilder} exact />
+      <Route path="/auth" component={Auth} />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if (isAuthenticated) {
+    output = (
+      <Switch>
+        <Route path="/" component={PizzaBuilder} exact />
+        <Route path="/orders" component={Orders} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+        <Redirect to="/" />
+      </Switch>
+    ); 
   }
+
+  return (
+    <div className="App">
+      <Layout>
+        {output}
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
