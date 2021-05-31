e3b724ec6539d5eda75a931faa49ea97143a6788
Your Name <you@example.com>
2021-05-31 13:42:18 +0600

    index js move

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers
+} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducer';
+import reportWebVitals from './reportWebVitals';
+import builder from './store/reducers/builder';
+import orders from './store/reducers/orders';
+import auth from './store/reducers/auth';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({ builder, orders, auth });
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
